cdab9c9b00ff30affab91300d185e2ca9959430b
Li Tian <li_tian@singaporeair.com.sg>
2020-09-26 23:30:17 +0800

    test on swift package, xcode 12

--- Package.swift
+++ Package.swift
@@ -1,5 +1,18 @@
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-  name: "ColorMatchTabs"
+  name: "ColorMatchTabs",
+  
+  platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "ColorMatchTabs",
+            targets: ["ColorMatchTabs"]),
+    ],
+    targets: [
+        .target(name: "ColorMatchTabs")
+    ]
 )
